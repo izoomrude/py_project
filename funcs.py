3983f88524872c1caa3e650b1e59108b876c708b
Аня <147255359+izoomrude@users.noreply.github.com>
2023-10-11 18:12:24 +0300

    add new funk

---
+++ funcs.py
@@ -0,0 +1,9 @@
+
+def funk_for_sum(num_1, num_2):
+    num_1 = int(input('Введите первое число: '))
+    num_2 = int(input('Введите второе число: '))
+    new_num = num_1 + num_2
+    return print(f'Сумма чисел: {new_num}')
+
+
+funk_for_sum(5, 10)

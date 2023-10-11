f40c9668d2b6ccf3212e72d8e62845d7adb05003
Аня <147255359+izoomrude@users.noreply.github.com>
2023-10-12 00:01:25 +0300

    add funk for subtraction

--- funcs.py
+++ funcs.py
@@ -6,4 +6,8 @@ def funk_for_sum(num_1, num_2):
     return print(f'Сумма чисел: {new_num}')
 
 
-funk_for_sum(5, 10)
+def funk_for_subtraction():
+    num_1 = int(input('Введите первое число: '))
+    num_2 = int(input('Введите второе число: '))
+    new_num = num_1 - num_2
+    return print(f'Разница чисел: {new_num}')
